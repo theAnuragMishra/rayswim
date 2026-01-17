0eae2e4ce68ff8c104877a75b1e1239ce7e838e6
Anurag <86455065+theAnuragMishra@users.noreply.github.com>
2026-01-18 01:33:01 +0530

    add scene

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,24 @@ use image::buffer::ImageBuffer;
 use math::vec3::Vec3;
 use ray::ray::Ray;
 use scene::hittable::Hittable;
+use scene::scene::Scene;
 
-fn ray_color(ray: &Ray, sphere: &Sphere) -> Vec3 {
-    if let Some(hit) = sphere.hit(ray, 0.001, f64::INFINITY) {
-        // color based on normal
+fn ray_color(ray: &Ray, scene: &Scene) -> Vec3 {
+    if let Some(hit) = scene.hit(ray, 0.001, f64::INFINITY) {
         return (hit.normal + Vec3::new(1.0, 1.0, 1.0)) * 0.5;
     }
-    // background gradient
     let unit_direction = ray.direction.normalized();
     let t = 0.5 * (unit_direction.y + 1.0);
     Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
-
 fn main() {
+    use std::sync::Arc;
+    let mut scene = Scene::new();
+    scene.add(Arc::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+    scene.add(Arc::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0))); // ground sphere
     let image_width = 400;
     let image_height = 200;
     let mut img = ImageBuffer::new(image_width, image_height);
-
-    let sphere = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
 
     // Camera setup
     let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
@@ -40,7 +40,7 @@ fn main() {
             let v = (image_height - 1 - j) as f64 / (image_height - 1) as f64;
             let direction = lower_left_corner + horizontal * u + vertical * v - origin;
             let r = Ray::new(origin, direction);
-            let color = ray_color(&r, &sphere);
+            let color = ray_color(&r, &scene);
             img.set_pixel(i, j, color);
         }
     }
--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,1 +1,2 @@
 pub mod hittable;
+pub mod scene;
